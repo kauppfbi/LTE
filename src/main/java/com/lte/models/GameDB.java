76a8f2a80058b471b4d3164081b8664e85798609
kauppfbi <kauppfbi@P01125038.schaeffler.com>
2016-10-19 21:48:14 +0200

    litlle update in GameDB

--- src/main/java/com/lte/models/GameDB.java
+++ src/main/java/com/lte/models/GameDB.java
@@ -29,6 +29,40 @@ public class GameDB {
 		this.replayTurns = replayTurns;
 	}
 
+
+	//Getter and Setters
+	public int getGameID() {
+		return gameID;
+	}
+
+	public void setGameID(int gameID) {
+		this.gameID = gameID;
+	}
+
+	public String getOpponentName() {
+		return opponentName;
+	}
+
+	public void setOpponentName(String opponentName) {
+		this.opponentName = opponentName;
+	}
+
+	public String getPlayTime() {
+		return playTime;
+	}
+
+	public void setPlayTime(String playTime) {
+		this.playTime = playTime;
+	}
+
+	public int getNumberOfSets() {
+		return numberOfSets;
+	}
+
+	public void setNumberOfSets(int numberOfSets) {
+		this.numberOfSets = numberOfSets;
+	}
+
 	public int getSetID() {
 		return setID;
 	}
@@ -37,83 +71,35 @@ public class GameDB {
 		this.setID = setID;
 	}
 
-	public int getNumberOfSets() {
-		return numberOfSets;
-	}
-
-
-	public void setNumberOfSets(int numberOfSets) {
-		this.numberOfSets = numberOfSets;
-	}
-
-
-	public int[] getReplayTurns() {
-		return replayTurns;
-	}
-
-
-	public void setReplayTurns(int[] replayTurns) {
-		this.replayTurns = replayTurns;
-	}
-
-
-	public int getGameID() {
-		return gameID;
-	}
-
-
-	public void setGameID(int gameID) {
-		this.gameID = gameID;
-	}
-
-
-	public String getOpponentName() {
-		return opponentName;
-	}
-
-
-	public void setOpponentName(String opponentID) {
-		this.opponentName = opponentName;
-	}
-
-
-	public String getPlayTime() {
-		return playTime;
-	}
-
-
-	public void setPlayTime(String playTime) {
-		this.playTime = playTime;
-	}
-
-
 	public int getPointsOpponent() {
 		return pointsOpponent;
 	}
-
 
 	public void setPointsOpponent(int pointsOpponent) {
 		this.pointsOpponent = pointsOpponent;
 	}
 
-
 	public int getPointsOwn() {
 		return pointsOwn;
 	}
-
 
 	public void setPointsOwn(int pointsOwn) {
 		this.pointsOwn = pointsOwn;
 	}
 
-
 	public String getWinner() {
 		return winner;
 	}
 
-
 	public void setWinner(String winner) {
 		this.winner = winner;
-	}	
-	
+	}
+
+	public int[] getReplayTurns() {
+		return replayTurns;
+	}
+
+	public void setReplayTurns(int[] replayTurns) {
+		this.replayTurns = replayTurns;
+	}
 }
