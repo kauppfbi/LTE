c6697bde2119cf36bee11e1f46ada708d89aa1ac
M66F <M66F@users.noreply.github.com>
2016-10-20 09:25:24 +0200

    Made method private again
    It is not supposed to be called from another class!

--- src/main/java/com/lte/db/DBconnection.java
+++ src/main/java/com/lte/db/DBconnection.java
@@ -299,7 +299,7 @@ public class DBconnection {
 	 * array[0] = 0 if we started, 1 of opponent started <br>
 	 * array[1..n] = columns of turns
 	 */
-	public int[] getReplayTurns(int GameID, int SetNumber){
+	private int[] getReplayTurns(int GameID, int SetNumber){
 		
 		int totalRows = 0;
 		int[] turns = null;
