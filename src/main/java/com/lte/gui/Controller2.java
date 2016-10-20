a6842f3b1a414ec735699228577458fbc8e51151
Seekerz1 <bauerflorian95@gmail.com>
2016-10-20 22:54:25 +0200

    Auf dem Stand fuer Felix zum verschoenern

--- src/main/java/com/lte/gui/Controller2.java
+++ src/main/java/com/lte/gui/Controller2.java
@@ -40,7 +40,8 @@ public class Controller2 {
 		this.controller = controller;
 	}
 	
-	SetDB[] sets; 
+	SetDB[] sets;
+	GameDB[] games;
 	int gameID;
 	int opponentID = 0;
 	String playTime = null;
@@ -127,6 +128,18 @@ public class Controller2 {
 		
 		int[] recTurns = sets[recSetNumber].getReplayTurns();
 		System.out.println("RecTurns:" + recTurns);
+		
+		String pointsOpponent = String.valueOf(sets[recSetNumber].getPointsOpponent());
+		String pointsOwn = String.valueOf(sets[recSetNumber].getPointsOwn());
+		
+		String nameOpponent = games[gameID].getOpponentName() ;
+		String nameOwn = "LTE";
+		
+		String numberAllSets = String.valueOf(games[gameID].getNumberOfSets());
+		String numberCurrentSet = String.valueOf(recSetNumber);
+		
+		metaText.setText(nameOwn + " " + pointsOwn + " | " + pointsOpponent + " " + nameOpponent + "    " + numberCurrentSet + "/" + numberAllSets);
+		
 		fillRec(recTurns);
 	}
 	
@@ -201,16 +214,16 @@ public class Controller2 {
 	// Methode zum Befuellen der Choice Boxes und zum Anfuegen der ChangeListeners
 	public void getRecGameInfo(){
 	
-		GameDB[] recGame = controller.getRecGameInfo();
+		games = controller.getRecGameInfo();
 		// Shown content in gameChoiceBox, Game Info (opponentplayer und playtime)
 		ObservableList<String> gameInfo = FXCollections.observableArrayList();
 		// Connection between content and gameID
 		TreeMap<Integer, Integer> connection = new TreeMap<Integer, Integer>();
 		
-		for(int i = 0; i < recGame.length; i++){
-			int gameID = recGame[i].getGameID();
+		for(int i = 0; i < games.length; i++){
+			int gameID = games[i].getGameID();
 			System.out.println(gameID);
-			gameInfo.add(recGame[i].getOpponentName().concat(" | ").concat(recGame[i].getPlayTime()));
+			gameInfo.add(games[i].getOpponentName().concat(" | ").concat(games[i].getPlayTime()));
 			connection.put(i, gameID);
 		}
 
