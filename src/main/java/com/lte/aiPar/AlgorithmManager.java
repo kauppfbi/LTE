d7e68a9e23052ee4ae029fa5b6d7cffc25ea8cc8
fabiansoelker <fabian.soelker@gmx.de>
2016-10-31 10:37:51 +0100

    Exception fix

--- src/main/java/com/lte/aiPar/AlgorithmManager.java
+++ src/main/java/com/lte/aiPar/AlgorithmManager.java
@@ -1,9 +1,9 @@
 package com.lte.aiPar;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -58,35 +58,35 @@ public class AlgorithmManager {
 			//Laufzeitanalyse starten
 			final long timeStart = System.currentTimeMillis(); 
 			
-			//Arrays l�schen
+			//Arrays lï¿½schen
 			list.clear();
 			future.clear();
 			
-			//H�chstwert und Spalte
+			//Hï¿½chstwert und Spalte
 			int highest = -1000000;
 			
 			//Neuen Spielstand aufbauen
 			GameScore parAlgGameScore = new GameScore(field);
 			
-			//M�gliche Z�ge
+			//Mï¿½gliche Zï¿½ge
 			byte[] moves = parAlgGameScore.possibleMoves();
 			
 			//Die ersten Runden beschleunigen
 			roundCounter++;
 
 			if(roundCounter <= 7){
-				algorithmDepth = 8;
+				algorithmDepth = 12;
 			}
 			
 			System.out.println(algorithmDepth);
 			
-			//Pr�fe ob nur noch ein Zug vorhanden ist
+			//Prï¿½fe ob nur noch ein Zug vorhanden ist
 			if(moves[1] == 99 && moves[0] != 99)
 			{
 				return moves[0];
 			}
 			
-			//Pr�fe ob in den n�chsten Zuegen gewonnen werden kann
+			//Prï¿½fe ob in den nï¿½chsten Zuegen gewonnen werden kann
 			SingleAlgorithm simpleAlg = new SingleAlgorithm(field, 4);
 			int simpleMove = simpleAlg.alphaBeta();
 			parAlgGameScore.play(simpleMove, playerByte);
@@ -103,7 +103,7 @@ public class AlgorithmManager {
 			}
 			
 			
-			//Spiele die ersten Z�ge und erzeuge neue Tasks
+			//Spiele die ersten Zï¿½ge und erzeuge neue Tasks
 			for (int i = 0; i < moves.length; i++) {
 				if(moves[i] == 99){break;}
 				parAlgGameScore.play(moves[i], playerByte);
@@ -118,11 +118,21 @@ public class AlgorithmManager {
 			calculationTime = (calculationTime * 1000) - 300;
 			if(calculationTime < 300){calculationTime = 300;}
 			
+			//Hï¿½chsten suchen
+			int counter = 0;
+			int column = 0;
+			ArrayList<Integer> results = new ArrayList<>();
+			
 			//Invoke + Timeout
 			try{
 				future = service.invokeAll(list, (long)calculationTime, TimeUnit.MILLISECONDS);
+				
+				//In ArrayList ï¿½bertragen
+				for(Future<?> obj : future){
+					results.add((int)obj.get());
+				}
 			}
-			catch (InterruptedException e) {
+			catch (CancellationException e) {
 				SingleAlgorithm timeoutAlg = new SingleAlgorithm(field, 6);
 				int timeoutMove = timeoutAlg.alphaBeta();
 				System.out.println("Timeout!");
@@ -130,18 +140,8 @@ public class AlgorithmManager {
 			}
 
 			
-			//H�chsten suchen
-			int counter = 0;
-			int column = 0;
-			ArrayList<Integer> results = new ArrayList<>();
-			
-			//In ArrayList �bertragen
-			for(Future<?> obj : future){
-				results.add((int)obj.get());
-			}
-			
-			//Pr�fe ob im n�chsten Zug verloren wird
-			//Ansonsten w�hle h�chsten als neue M�glichkeit
+			//Prï¿½fe ob im nï¿½chsten Zug verloren wird
+			//Ansonsten wï¿½hle hï¿½chsten als neue Mï¿½glichkeit
 			for (int i = 0; i < results.size(); i++) {
 		    	
 			    if(results.get(i) >= highest){
@@ -154,7 +154,7 @@ public class AlgorithmManager {
 			    	
 			    	}
 			    	else{
-			    		System.out.println("Gef�hrliche Stellung in Spalte erkannt: " + moves[counter]);
+			    		System.out.println("Gefï¿½hrliche Stellung in Spalte erkannt: " + moves[counter]);
 			    	}
 			    	parAlgGameScore.unDo(moves[counter]);
 			    	}
@@ -171,7 +171,7 @@ public class AlgorithmManager {
 			final long timeEnd2 = System.currentTimeMillis(); 
 	        System.out.println("Laufzeit KI: " + (timeEnd2 - timeStart) + " Millisek.");
 			
-			//Spalte zur�ck geben
+			//Spalte zurï¿½ck geben
 			return column;
 		}
 		
