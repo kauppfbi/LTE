5d262587f8dbbd041471d59fd836c08cc9e7e4b6
kauppfbi <kauppfbi@P01125038.schaeffler.com>
2016-11-02 11:52:34 +0100

    remove unused import

--- src/main/java/com/lte/interfaces/FileIM.java
+++ src/main/java/com/lte/interfaces/FileIM.java
@@ -12,7 +12,6 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.XMLOutputter;
 
 public class FileIM implements InterfaceManager {
 
