2cc8b8308c2790b16c76771df2d363d4cfce7510
kauppfbi <kauppfbi@P01125038.schaeffler.com>
2016-11-13 13:02:16 +0100

    additional changes :'( :D

--- src/main/java/com/lte/gui/ControllerPlayerKi.java
+++ src/main/java/com/lte/gui/ControllerPlayerKi.java
@@ -365,7 +365,6 @@ public class ControllerPlayerKi {
 						try {
 							threadPlayerKi.setNextMove(-1);
 						} catch (Exception e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 						threadPlayerKi.notify();
